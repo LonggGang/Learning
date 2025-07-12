c810322d8e69a38c9e30f58bd579f1ed52e2b384
long96 <longhoang96260@gmail.com>
2025-07-12 20:31:15 +0700

    Change folder name

---
+++ Machine Learning/C1 - Week1/C1_W1_Lab03_Cost_function_Soln.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()  # Turns on interactive mode
+import matplotlib.pyplot as plt
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+x_train = np.array([1.0, 2.0])        
+y_train = np.array([300.0, 500.0])    
+def compute_cost(x,y,w,b):
+    m = x.shape[0]
+    cost_sum = 0
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = (f_wb - y[i])**2
+        cost_sum = cost_sum + cost
+    total_cost = (1/(2*m)) * cost_sum
+    return total_cost
+plt_intuition(x_train,y_train)
